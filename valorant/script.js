f6953bd3c1ee4cb8fd373cebb63fadbfc2aa201e
ValanyrTV <117078171+ValanyrTV@users.noreply.github.com>
2022-11-24 18:57:40 +0100

    Add files via upload

---
+++ valorant/script.js
@@ -0,0 +1,65 @@
+function Europe() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "900 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function SouthKorea() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "650 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function NorthAmerica() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "748 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Cis() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "640 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Turkey() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "875 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Japan() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "754 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Brazil() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "682 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Latam() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "427 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function AsiaPacific() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "894 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Oceania() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "741 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
+
+function Mena() {
+    text = document.getElementById('txt_vide').innerHTML;
+    txt = text.replace(text, "359 000 joueurs actifs quotidiennement sur Valorant !");
+    document.getElementById("txt_vide").innerHTML = txt;
+}
